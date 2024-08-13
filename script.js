9b9df5a61503fcdabb89bf1cf4a22d06f7a414eb
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-13 20:27:01 +0200

    Design form

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const addBtn = document.querySelector(".btn")
+const submitBtn = document.querySelector(".submit")
 const form = document.querySelector(".formInput")
 
 addBtn.addEventListener("click", ()=>{
     form.style.opacity = 1
     form.style.transform = 'translateY(100px)'
+})
+
+submitBtn.addEventListener("click",()=>{
+    form.style.opacity = 0
+    form.style.transform = 'translateY(-10px)'
 })
