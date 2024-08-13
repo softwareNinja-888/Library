2f38272a1b591b443b65b8d1e24232f084aeea4b
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-13 21:51:06 +0200

    Add close button to form

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
     const title = document.querySelector(".title")
     const pages = document.querySelector(".pages")
     const read = document.querySelector(".read")
+    const close = document.querySelector(".closeImg")
 
     // CODE:
 
@@ -25,6 +26,13 @@
     function addBook(author,title,pages,read){
        const item = new Book(author,title,pages,read)
        library.push(item)
+    }
+
+    function clearFields(){
+        author.value = ''
+        title.value = ''
+        pages.value = ''
+        read.checked = false
     }
 
     // addBook("James","Jeff Dun",456,false)
@@ -46,11 +54,17 @@
 
     submitBtn.addEventListener("click",()=>{
         
-        addBook(author.value,title.value,pages.value,read.checked)       
-        form.style.opacity = 0
+        addBook(author.value,title.value,pages.value,read.checked)  
+        clearFields();  
+        form.style.opacity = 0;
         form.style.transform = 'translateY(-1000px)'
     })
 
+    close.addEventListener("click", ()=>{
+        clearFields();  
+        form.style.opacity = 0;
+        form.style.transform = 'translateY(-1000px)'
+    })
     
 })()
 
