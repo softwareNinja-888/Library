9893e4ac6eef95d1f5cfa95dff3b7a42a25e8275
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-13 20:00:58 +0200

    Add form structure and design

---
+++ script.js
@@ -0,0 +1,7 @@
+const addBtn = document.querySelector(".btn")
+const form = document.querySelector(".formInput")
+
+addBtn.addEventListener("click", ()=>{
+    form.style.opacity = 1
+    form.style.transform = 'translateY(100px)'
+})
