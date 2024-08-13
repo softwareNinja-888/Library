45dbf71272dd76e30f4eefe41da76f95438c2e54
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-13 20:55:38 +0200

    Add objects and functions
    Added the constructor that I will use to create
    the books

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-const addBtn = document.querySelector(".btn")
-const submitBtn = document.querySelector(".submit")
-const form = document.querySelector(".formInput")
+(function Start(){
+    
+    //  SELECTING :
 
-addBtn.addEventListener("click", ()=>{
-    form.style.opacity = 1
-    form.style.transform = 'translateY(100px)'
-})
+    const addBtn = document.querySelector(".btn")
+    const submitBtn = document.querySelector(".submit")
+    const form = document.querySelector(".formInput")
 
-submitBtn.addEventListener("click",()=>{
-    form.style.opacity = 0
-    form.style.transform = 'translateY(-10px)'
-})
+    // EVENT LISTERNERS:
+
+    addBtn.addEventListener("click", ()=>{
+        form.style.opacity = 1
+        form.style.transform = 'translateY(100px)'
+    })
+
+    submitBtn.addEventListener("click",()=>{
+        form.style.opacity = 0
+        form.style.transform = 'translateY(-1000px)'
+    })
+
+    // CODE:
+
+    let library = []
+
+    function Book (author,title,pages,read){
+        this.author = author
+        this.title = title
+        this.pages = pages
+        this.read = read
+    }
+
+    function addBook(author,title,pages,read){
+       const item = new Book(author,title,pages,read)
+       library.push(item)
+    }
+
+    // addBook("James","Jeff Dun",456,false)
+    // addBook("Youll","James Bond", 321, true)
+
+    // console.log(library);
+
+    // function displayLibrary(){
+
+    // } 
+    
+})()
+
