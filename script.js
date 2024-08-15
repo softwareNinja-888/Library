d592326b7ed52bbcfef187ba0f4190547de40596
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-08-15 18:22:35 +0200

    Refactor code

--- script.js
+++ script.js
@@ -15,9 +15,13 @@
     const cards = document.querySelector(".cards")
     const arr = ["author","title","pages","read"]
 
+    const overlay = document.querySelector(".modal-overlay")
+
     // CODE:
 
     let library = []
+
+    // OBJECT CONSTRICTOR FOR BOOKS 
 
     function Book (author,title,pages,read){
         this.author = author
@@ -26,12 +30,16 @@
         this.read = read
     }
 
+    // CREATE BOOK AND ADD TO LIBRARY ARRAY
+
     function addBook(author,title,pages,read){
        const item = new Book(author,title,pages,read)
        library.push(item)
        displayBook(library)
        
     }
+
+    // LOOP OVER LIBRARY TO DISPLAY BOOKS
 
     function displayBook(library){
 
@@ -43,6 +51,8 @@
             createCard(e)
         })
     }
+
+    // CREATES CARDS FOR EACH BOOK IN LIBRARY
 
     function createCard(info){
         const n = 4
@@ -59,12 +69,16 @@
             let p = document.createElement("p")
             let pAns =document.createElement("p")
 
-            p.textContent = `${lowArr(arr,i)}: `
-            pAns.textContent = info[`${arr[i]}`]
+            p.textContent = `${lowArr(arr,i)}: `;
+            
+            // DYNAMICALLY ADD READING STATUS
+
+            (typeof info[`${arr[i]}`] === 'boolean') ? pAns.textContent = info[`${arr[i]}`] ? "Done Reading": "Not yet" : pAns.textContent = info[`${arr[i]}`]
 
 
             p.classList.add(`display${lowArr(arr,i)}`)
             pAns.classList.add(`${arr[i]}Ans`)
+            pAns.classList.add('font-semibold')
 
 
             newDiv.appendChild(p)
@@ -90,9 +104,9 @@
         card.appendChild(btnCon)
         cards.appendChild(card)
 
-        console.log(card);
-
     }
+
+    // CLEAR FIELDS OF FORM 
 
     function clearFields(){
         author.value = ''
@@ -103,23 +117,34 @@
 
     // EVENT LISTERNERS:
 
+    // ADD BOOK BUTTON POPS UP MODAL
+
     addBtn.addEventListener("click", ()=>{
-        form.style.opacity = 1
-        form.style.transform = 'translateY(100px)'
+        form.classList.add("showing")
+        overlay.classList.add("showing")
+        author.focus()
+        author.required = true
+        author.setAttribute("required","")
     })
+    
+    // SUBMIT BUTTON ADDBOOK AND REMOVES MODAL
 
     submitBtn.addEventListener("click",()=>{
         
         addBook(author.value,title.value,pages.value,read.checked)  
         clearFields();  
-        form.style.opacity = 0;
-        form.style.transform = 'translateY(-1000px)'
+        form.classList.remove("showing")
+        overlay.classList.remove("showing")
+
     })
+
+    // CLOSES MODAL AND CLEARS FIELDS
 
     close.addEventListener("click", ()=>{
         clearFields();  
-        form.style.opacity = 0;
-        form.style.transform = 'translateY(-1000px)'
+        form.classList.remove("showing")
+        overlay.classList.remove("showing")
+
     })
     
 })()
